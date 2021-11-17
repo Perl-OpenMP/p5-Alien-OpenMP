cfb7d66e3632301f28ca2adf9d6642d31940c57f
Ed J <mohawk2@users.noreply.github.com>
2021-11-17 01:03:32 +0000

    Bump version

--- lib/Alien/OpenMP.pm
+++ lib/Alien/OpenMP.pm
@@ -4,7 +4,7 @@ use parent 'Alien::Base';
 use Config ();
 use Alien::OpenMP::configure ();
 
-our $VERSION = '0.003006';
+our $VERSION = '0.003007';
 
 # "public" Alien::Base method implementations
 
